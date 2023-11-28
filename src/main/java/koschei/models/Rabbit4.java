5dc312558346af1e721703c4babb7dfffc53ac7d
Hozyain <qwpo01_0101@mail.ru>
2023-11-28 20:48:28 +0400

    Задание выполнено

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -7,9 +7,6 @@ import org.springframework.stereotype.Component;
 public class Rabbit4 {
     @Autowired
     private Duck5 duck5;
-    public Rabbit4(Duck5 duck5){
-        this.duck5 = duck5;
-    }
     @Override
     public String toString() {
         return ", в зайце утка " + duck5.toString();
